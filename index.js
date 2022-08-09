599547ded89786b0f49c5e2a1b1c1e4c44e466c7
TerenceChew <wschew97@gmail.com>
2022-08-09 04:51:17 -0400

    Add clear displays and backspace buttons

--- index.js
+++ index.js
@@ -36,17 +36,43 @@ function operate(operator, n1, n2) {
   return result;
 }
 
-const buttons = document.querySelectorAll('.displayed');
+// Value of these buttons will get displayed when clicked
+const displayedBtns = document.querySelectorAll('.displayed');
 const display1 = document.querySelector('.display1');
 let display1Content;
 
-buttons.forEach(btn => btn.addEventListener('click', displayBtnValue));
+displayedBtns.forEach(btn => btn.addEventListener('click', displayBtnValue));
 
 function displayBtnValue(e) {
   const btnValue = e.target.textContent;
   const oldDisplay1Content = display1.textContent;
-  const newDisplay1Content = oldDisplay1Content + btnValue;
+  const newDisplay1Content = (oldDisplay1Content + btnValue).trim();
   display1.textContent = newDisplay1Content;
   display1Content = newDisplay1Content;
-  console.log(display1Content.trim());
+  console.log(display1Content);
+}
+
+// Clear displays upon button click
+const display2 = document.querySelector('.display2');
+let display2Content;
+const acBtn = document.querySelector('.ac');
+
+acBtn.addEventListener('click', clearDisplays);
+
+function clearDisplays() {
+  display1.textContent = '';
+  display1Content = '';
+  display2.textContent = '';
+  display2Content = '';
+}
+
+// Backspace upon button click
+const delBtn = document.querySelector('.del');
+delBtn.addEventListener('click', backspace);
+
+function backspace() {
+  const newDisplay1Content = display1Content.slice(0, -1);
+  display1.textContent = newDisplay1Content;
+  display1Content = newDisplay1Content;
+  console.log(display1Content);
 }
